2650158b379fa7ee3222f35f1d6564765b2622fb
Samantha Britney <sambritney@gmail.com>
2016-07-18 16:56:22 -0700

    Modified Enemy class & update function. Created Player class, update, render and handleInput functions. Modified player sprite image /app and engine files/

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +29,42 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function (x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-horn-girl.png';
+};
+
+Player.prototype.update = function() {
+    
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    if (key === 'left'){
+        this.x -= 100;
+    }
+    if (key === 'right') {
+        this.x += 100;
+    }
+    if (key === 'up') {
+        this.y -= 100;
+    }
+    if (key === 'down') {
+        this.y += 100;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [new Enemy(0, 60, 10), new Enemy(0, 145, 20), new Enemy (0, 230, 30)];
+
+var player = new Player(200, 400);
 
 
 // This listens for key presses and sends the keys to your
