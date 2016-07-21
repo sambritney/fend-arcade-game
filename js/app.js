2f9bd92b26580d89613c37f368ba84a3db2d145d
Samantha Britney <sambritney@gmail.com>
2016-07-21 12:05:45 -0700

    Instantiated Player and Enemy classes Reset bugs for continuous flow in update function Confined player movement to screen in update function Added collision detection to Enemy constructor class, check collisions in update Added player reset method in Player class Added reset to player update function if player reaches water

--- js/app.js
+++ js/app.js
@@ -11,6 +11,15 @@ var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
+//Enemy and player collision function
+Enemy.prototype.checkCollision = function(player) {
+    if (player.x < this.x +75 &&
+        player.x + 75 > this.x &&
+        player.y < this.y + 20 &&
+        player.y + 20 > this.y) {
+        player.reset();
+    }
+};
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -18,7 +27,13 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
+    if (this.x > 505) {
+        this.x = 0
+    }
+    this.checkCollision(player);
 };
+
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -35,36 +50,57 @@ var Player = function (x, y) {
     this.sprite = 'images/char-horn-girl.png';
 };
 
+// The update function confines the player to the screen and calls reset when the player reaches the water
 Player.prototype.update = function() {
-    
+    if (this.x < 0) {
+        this.x = 0
+    }
+    else if (this.x > 404) {
+        this.x = 404
+    }
+    else if (this.y < 73) {
+        alert ("You made it!");
+        this.reset()
+    }
+    else if (this.y > 405) {
+        this.y = 405
+    }
 };
 
 Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// This handles player movement using the allowedKeys event listener
 Player.prototype.handleInput = function(key) {
     if (key === 'left'){
-        this.x -= 100;
+        this.x -= 101;
     }
     if (key === 'right') {
-        this.x += 100;
+        this.x += 101;
     }
     if (key === 'up') {
-        this.y -= 100;
+        this.y -= 83;
     }
     if (key === 'down') {
-        this.y += 100;
+        this.y += 83;
     }
 };
+
+// Reset function for player for when water is reached or collision detected 
+Player.prototype.reset = function(player) {
+    this.x = 202;
+    this.y = 405;
+};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [new Enemy(0, 60, 10), new Enemy(0, 145, 20), new Enemy (0, 230, 30)];
+var player = new Player(202, 405);
 
-var player = new Player(200, 400);
+var allEnemies = [new Enemy(0, 62, 300), new Enemy(0, 145, 100), new Enemy (0, 228, 400)];
 
 
 // This listens for key presses and sends the keys to your
